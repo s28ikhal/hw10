3c883b18f842f380ebba971d8579c20ebf17bea5
s28ikhal <khalimov1@jrlogin06.jureca>
2024-01-17 23:21:10 +0100

    Add 1.c

--- src/10_01_c.c
+++ src/10_01_c.c
@@ -23,23 +23,23 @@ int main(int argc, char ** argv)
   // in principle almost every MPI function returns an integer to communicate
   // the success or failure of the operation
   int mpi_error;
-  //mpi_error = MPI_Init(...);
+  mpi_error = MPI_Init(&argc, &argv);
   check_mpi_error(mpi_error, __FILE__, __LINE__-1);
 
-  int rank = 0;
+  int rank = 0; 
   int n_ranks = 1;
-  //MPI_Comm_rank(...);
-  //MPI_Comm_size(...);
-
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
+  
   // do not modify message
   const int message = rank * 11;
   int recv_buffer;
   MPI_Status mpi_status;
 
-  int rank_to;
-  int rank_from;
+  int rank_to = (rank + 1) % n_ranks;
+  int rank_from = (rank - 1) % n_ranks;
 
-  //MPI_Sendrecv(..., &mpi_status);
+  MPI_Sendrecv(&message, 1, MPI_INT, rank_to, 0, &recv_buffer, 1, MPI_INT, rank_from, 0, MPI_COMM_WORLD, &mpi_status);
 
   printf("Rank %d sent %d to rank %d and received %d from rank %d, tagged %d with error value %d\n",
          rank, message, rank_to, recv_buffer, mpi_status.MPI_SOURCE, mpi_status.MPI_TAG, mpi_status.MPI_ERROR);
